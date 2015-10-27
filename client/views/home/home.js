603ad7cb7e356ea265841383b3d6d59f479c3e71
Steve <steve@bittiger.io>
2015-10-27 10:30:34 -0400

    Notficiations working - pre meteor update commit

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -8,18 +8,20 @@ Template.home.events({
         $("#meditationBell").trigger("play");
         $("#meditationBell").trigger("pause");
         console.log("audio initialized");
-        /*
+
         if(Meteor.isCordova) {
             cordova.plugins.notification.local.schedule({
                 id: 1,
                 title: "Meditation Complete",
                 text: "Great Job",
-                at: new Date(),
-                sound: getMediaUrl('/sounds/meditationbell.mp3'),
-                icon: "http://icons.com/?cal_id=1"
+                badge: 1,
+                at: new Date((new Date()).getTime() + 60000),
+                sound: 'file://application/app/sounds/meditationbell.mp3',
+                ongoing:  true
+
             });
         }
-         */
+
         Router.go("selectTime");
 
     }
